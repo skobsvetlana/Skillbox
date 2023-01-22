cdff30d533691cdd076db4adf5f40f720be4c073
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-01-22 18:30:10 +0300

    changed 02_session/main.py fixed zero division error

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+def calculate_line_equation(x1, y1, x2, y2):
+    if x1 == x2:
+        return 'x = ' + str(x1)
+
+    if y1 == y2:
+        return 'y = ' + str(y1)
+
+    x_diff = x1 - x2
+    y_diff = y1 - y2
+
+    k = y_diff / x_diff
+    b = y2 - k * x2
+
+    if b < 0:
+        result = "y = " + str(k) + " * x - " + str(abs(b))
+    else:
+        result = "y = " + str(k) + " * x + " + str(b)
+
+    return result
+
+
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+result = calculate_line_equation(x1, y1, x2, y2)
+
+print("\nУравнение прямой, проходящей через эти точки:")
+print(result)
+
