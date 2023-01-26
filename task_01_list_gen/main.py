ad57b6c22d0658071b671ecad7365f563efbbe18
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-01-26 17:55:26 +0300

    task_01_list_gen changed

--- task_01_list_gen/main.py
+++ task_01_list_gen/main.py
@@ -1,53 +1,22 @@
-def get_input_parameters():
-    """
-    Получаем N
-
-    :return: N, например: 14
-    :rtype: int
-    """
-    # TODO: в этой функции пишем весь необходимый код для
-    #  получения входных параметров.
-    #  Логику расчётов тут не программируем
-    pass
+def get_input_parameters(number):
+    return int(number)
 
 
 def display_result(odd_numbers):
-    """
-    Выводим список нечётных чисел
-
-    :param odd_numbers: список нечётных чисел, например: [1, 3, 5, 7, 9, 11, 13]
-    :type odd_numbers: List[int]
-    """
-    # TODO: в этой функции пишем весь необходимый код
-    #  для вывода результата в нужном формате.
-    #  Логику расчётов тут не программируем
-    pass
+    print(odd_numbers)
 
 
 def get_odd_numbers(number):
-    """
-    Получаем отсортированный по возрастанию список
-    нечётных чисел от 1 до number.
+    result = []
 
-    :param number: до какого числа нужно рассчитать, например: 14
-    :type number: int
+    for i in range(number):
+        if i % 2 == 0:
+            result.append(i)
 
-    :return: список нечётных чисел, например: [1, 3, 5, 7, 9, 11, 13]
-    :rtype: List[int]
-    """
-    # TODO: в этой функции получаем отсортированный
-    #  по возрастанию список нечётных чисел от 1 до number.
-    #  print'ов и input'ов тут не должно быть.
-    #  Функция на вход принимает ранее полученные данные
-    #  (из функции get_input_parameters).
-    #  Функция на выход отдаёт результат необходимый для отображения работы программы,
-    #  который будет передан в функцию display_result.
-    pass
+    return result
 
 
 if __name__ == '__main__':
-    # Это условие необходимо, чтобы в рамках автотестов не произошёл
-    # вызов функций get_input_parameters и display_result
     number = get_input_parameters()  # получаем параметры
     odd_numbers = get_odd_numbers(number)  # получаем нечётные числа
     display_result(odd_numbers)  # выводим результат
