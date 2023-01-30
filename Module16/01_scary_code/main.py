e78c847e6c621fd84d6fcb3221b8421b8532a08b
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-01-30 18:44:22 +0300

    changed Module16/01_scary_code/main.py

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,20 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+list1 = [1, 5, 3]
+list2 = [1, 5, 1, 5]
+list3 = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+list1.extend(list2)
+
+print(f'Кол-во цифр 5 при первом объединении: {list1.count(5)}')
+
+while 5 in list1:
+    list1.remove(5)
+
+list1.extend(list3)
+print(f'Кол-во цифр 3 при втором объединении: {list1.count(3)}')
+
+print(f'Итоговый список: {list1}')
+
+
+
+
+
