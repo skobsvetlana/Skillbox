dee13e95081983b5d40fa8bae531ea7db9d1be6a
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-05 21:22:21 +0300

    changed Module17/03_random_competition/main.py

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import random
+
+squad1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+squad2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+winners = [squad1[i] if squad1[i] > squad2[i] else squad2[i]
+           for i in range(len(squad1))]
+
+print(f'Первая команда: {squad1}')
+print(f'Вторая команда: {squad2}')
+print(f'Победители тура: {winners}')
