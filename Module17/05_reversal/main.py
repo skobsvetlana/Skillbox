652095e4c21c9e2454ea3c2463bd8dde7a132196
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-05 22:04:49 +0300

    changed Module17/05_reversal/main.py

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+
+text = input('Введите строку: ')
+ind = [i for i in range(len(text)) if text[i] == 'h']
+res = text[ind[0] + 1: ind[len(ind) - 1]]
+
+print(f'Развёрнутая последовательность между первым и последним h: {res[::-1]}')
