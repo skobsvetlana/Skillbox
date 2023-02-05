e9a67c2cbc1e2e89b539db97302f5d5eaa537b2f
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-05 21:02:48 +0300

    changed Module17/01_vowels/main.py

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+vowels = 'аеёиоуыэюя'
+text = input('Введите текст: ')
+vowels_list = [x for x in text if x in vowels]
+
+print(f'Список гласных букв: {vowels_list}')
+print(f'Длина списка: {len(vowels_list)}')
+
+
+
