1043d8e0addbccbbc2eb252e10ee58362916d00f
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-06 20:15:16 +0300

    changed Module17/09_list_of_lists_2/main.py

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,6 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+flat_list =[el for sub_list2 in nice_list for sub_list1 in sub_list2 for el in sub_list1]
+
+print(f'Ответ: {flat_list}')
