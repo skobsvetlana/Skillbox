ab06a6236e618c140deb9ffcecd66bd843dc8170
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-07 13:42:26 +0300

    changed Module17/10_caesar_cipher/main.py

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -3,22 +3,15 @@ def new_el(el, shift):
     alphabet = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
 
     if el in alphabet:
-        new_ind = alphabet.index(el) + shift
-        if new_ind > 32:
-            new_ind -= 33
+        new_ind = (alphabet.index(el) + shift) % 33
+        print(alphabet.index(el), new_ind)
         el = alphabet[new_ind]
 
     return el
 
 
-# text = input('Введите сообщение: ')
-# shift = int(input('Введите сдвиг: '))
-
-
-
-
-text = list('это питон.')
-shift = 3
+text = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
 text = [new_el(el, shift) for el in text]
 
 print(f'Зашифрованное сообщение: {"".join(text)}')
