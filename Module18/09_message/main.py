503adf69005e182ce8691798cf8a42bb7a66995f
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-09 23:22:06 +0300

    changed Module18/09_message/main.py

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def reversed_word(word):
+    res = []
+    spam = []
+
+    for sym in word:
+        if sym.isalpha():
+            spam.append(sym)
+        else:
+            res.append(''.join(spam[::-1]))
+            res.append(sym)
+            spam = []
+    res.append(''.join(spam[::-1]))
+
+    return ''.join(res)
+
+
+text = input('Сообщение: ').split()
+res = []
+
+for word in text:
+    if word.isalpha():
+        res.append(word[::-1])
+    else:
+        res.append(reversed_word(word))
+
+res = ' '.join(res)
+
+print(res)
