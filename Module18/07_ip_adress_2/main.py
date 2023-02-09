4fadaaab46180647f97f1c502c4b2bfe19829830
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-09 21:17:03 +0300

    changed Module18/07_ip_address/main.py

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def symbol_checkout(sym):
+    for el in sym:
+        if el.isalpha():
+            return 1
+    else:
+        return 2
+
+
+ip = input('Введите IP: ').split('.')
+
+for el in ip:
+    if el.isdigit():
+        if int(el) > 255:
+            print(f'{el} превышает 255.')
+            break
+    else:
+        res = symbol_checkout(el)
+
+        if res == 1:
+            print(f'{el} — это не целое число.')
+            break
+        elif res == 2:
+            print('Адрес — это четыре числа, разделённые точками.')
+            break
+else:
+    print('IP-адрес корректен.')
+
+
