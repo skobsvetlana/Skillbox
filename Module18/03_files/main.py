b800b1d0659007e72146e1b7051c52550a366cb1
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-09 19:49:29 +0300

    changed Module18/03_files/main.py

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -6,3 +6,16 @@ def is_spec_symbols(name):
             return True
 
     return False
+
+
+file_extension = ['.txt', '.docx']
+file_name = input('Название файла: ')
+
+if is_spec_symbols(file_name):
+    print('Ошибка: название начинается на один из специальных символов.')
+
+elif file_name.endswith('.txt') or file_name.endswith('.docx'):
+    print('Файл назван верно.')
+
+else:
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx.')
