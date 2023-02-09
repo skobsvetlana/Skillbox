da633db281e07cc2ef42ca2ec7d6e6d9594279d4
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-09 19:48:35 +0300

    changed Module18/03_files/main.py

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def is_spec_symbols(name):
+    spec_symbols = '@№$%^&*()'
+
+    for symbol in spec_symbols:
+        if file_name.startswith(symbol):
+            return True
+
+    return False
