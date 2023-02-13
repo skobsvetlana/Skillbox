f2347004ba7826ea98c599b854236096720a6a55
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-13 20:17:41 +0300

    changed Module19/07_pizza/main.py

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+n = int(input('Введите количество заказов: '))
+orders = dict()
+
+for i in range(n):
+    print('{}-ый заказ: '.format(i + 1), end='')
+    order = input('').split()
+    order[2] = int(order[2])
+
+    if order[0] in orders:
+        if order[1] in orders[order[0]]:
+            orders[order[0]][order[1]] += order[2]
+        else:
+            orders[order[0]][order[1]] = order[2]
+    else:
+        orders[order[0]] = {order[1] : order[2]}
+
+for name, pizzas in orders.items():
+    print('{}:'.format(name))
+    for pizza, num in pizzas.items():
+        print('\t{}:{}'.format(pizza, num))
