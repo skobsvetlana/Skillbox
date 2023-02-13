83663275df136758b82ce44d3c27ebf166dcaaed
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-13 23:49:32 +0300

    changed Module19/10_palindrome/main.py

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+n = int(input('Введите количество человек: '))
+tree = dict()
+
+for i in range(n - 1):
+    print('{}-ая пара: '.format(i + 1), end='')
+    pair = input().split()
+
+print(tree)
