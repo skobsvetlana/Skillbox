db4558ce6020295f9f1ad7916b5ca0b6d97ba247
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-14 15:35:06 +0300

    changed Module19/09_pedigree/main.py

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -4,5 +4,16 @@ tree = dict()
 for i in range(n - 1):
     print('{}-ая пара: '.format(i + 1), end='')
     pair = input().split()
+    if pair[1] in tree:
+        tree[pair[0]] = tree[pair[1]] + 1
+    else:
+        tree[pair[1]] = 0
+        tree[pair[0]] = tree[pair[1]] + 1
 
-print(tree)
+for key, value in sorted(tree.items()):
+    print(key, value)
+
+
+
+
+
