042560665b709424c1a731c169404c58a4b929f7
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-15 23:10:58 +0300

    changed Module20/10_my_zip/main.py

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+my_str = 'abcd'
+tpl = (10, 20, 30, 40)
+
+n = min(len(my_str), len(tpl))
+res = ((my_str[i], tpl[i]) for i in range(n))
+
+print(res)
+
+for el in res:
+    print(el)
+
