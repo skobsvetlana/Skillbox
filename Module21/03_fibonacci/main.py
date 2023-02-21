76ed13970da95fa26de86895ff2919deb5abfd06
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-21 21:29:33 +0300

    changed Module21/03_fibonacci/main.py

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+num_pos = int(input('Введите позицию числа в ряде Фибоначчи: '))
+res = fibonacci(num_pos)
+
+print(f'Число: {res}')
