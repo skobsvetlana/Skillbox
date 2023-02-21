94b5d2777a7dad2d18024ec9998f109df8e610a7
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-21 22:26:53 +0300

    changed Module21/04_search_element_2/main.py

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,44 @@
-# TODO здесь писать код
+def find_value_by_key(data, my_key, deep=0):
+    if my_key in data:
+        return data[my_key]
+
+    res = None
+
+    for value in data.values():
+        if deep == 1:
+            break
+
+        if isinstance(value, dict):
+            res = find_value_by_key(value, my_key, deep - 1)
+            if res:
+                break
+
+    return res
+
+
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+key_to_find = input('Введите искомый ключ: ')
+deep = input('Хотите ввести максимальную глубину? Y/N: ').lower()
+
+if deep == 'y':
+    deep = int(input('Введите максимальную глубину: '))
+else:
+    deep = 0
+
+
+my_value = find_value_by_key(site, key_to_find, deep)
+
+print('Значение ключа: {}'.format(my_value))
+
