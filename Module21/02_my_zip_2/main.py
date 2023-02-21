bfb833dd82eb8f2225153752b0d96da7f2451b4e
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-21 21:28:26 +0300

    changed Module21/02_my_zip_2/main.py

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def my_zip(res, *arg):
+    if [] not in arg:
+        arg = [list(x) for x in arg]
+        res.append(tuple([x.pop(0) for x in arg]))
+        my_zip(res, *arg)
+
+
+res = []
+my_zip(res, {'a': 1, 'b': 2, 'c': 3}, [1, 2, 3, 4, 5], (3, 4, 5), "asdf")
+
+#print(res)
