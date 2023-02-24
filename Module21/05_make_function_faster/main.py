0a192eb17291fed17b7c400559cac0385591f1c0
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-24 20:29:22 +0300

    changed Module21/04_deep_copy/main.py

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,29 @@
+# def calculating_math_func(data):
+#     result = 1
+#     for index in range(1, data + 1):
+#         result *= index
+#     result /= data ** 3
+#     result = result ** 10
+#     return result
+
+
+def factorial(n):
+    if n < len(factorial_list):
+        return factorial_list[n]
+
+    if n == 0:
+        return 1
+
+    res = n * factorial(n - 1)
+    factorial_list.append(res)
+    return res
+
+
 def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+    result = factorial(data)
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+factorial_list = [1]
+
--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -4,11 +4,41 @@ site = {
             'title': 'Куплю/продам телефон недорого'
         },
         'body': {
-            'h2': 'У нас самая низкая цена на iphone',
+            'h2': 'У нас самая низкая цена на телефон',
             'div': 'Купить',
             'p': 'продать'
         }
-    }
+     }
 }
 
-# TODO здесь писать код
+def make_site(site, original, replacement):
+    for key, value in site.items():
+        if isinstance(value, dict):
+            make_site(value, original, replacement)
+        else:
+            if original in value:
+                site[key] = value.replace(original, replacement)
+
+
+import copy
+
+n = int(input('Сколько сайтов: '))
+original = 'телефон'
+
+for _ in range(n):
+    site_copy= copy.deepcopy(site)
+    replacement = input('Введите название продукта для нового сайта: ')
+
+    make_site(site_copy, original, replacement)
+
+    print(f'Сайт для {replacement}: \n')
+    print(site_copy)
+
+
+
+
+
+
+
+
+
