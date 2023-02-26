5981211782cf9b8990ad9834771381665602260a
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-26 19:06:27 +0300

    changed Module21/08_list_of_lists_2/main.py

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,14 +1,14 @@
-def flat(my_list):
+def flat_list(my_list):
     if not my_list:
         return []
 
     if isinstance(my_list[0], list):
-        return flat(my_list[0]) + flat(my_list[1:])
+        return flat_list(my_list[0]) + flat_list(my_list[1:])
     else:
-        return my_list[:1] + flat(my_list[1:])
+        return my_list[:1] + flat_list(my_list[1:])
 
 
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-print(flat(nice_list))
+print(flat_list(nice_list))
