7839a3dd484e7a9142ad958230271518a9741aca
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-26 18:58:46 +0300

    changed Module21/07_advanced_sum/main.py

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,5 +1,23 @@
-def my_sum(*args):
-    print(type(args))
+def flat(my_list):
+    if not my_list:
+        return []
+
+    if isinstance(my_list[0], list):
+        return flat(my_list[0]) + flat(my_list[1:])
+    else:
+        return my_list[:1] + flat(my_list[1:])
 
 
-print(my_sum(1, 2, (3, 4), 5))
+def my_sum(*args, res=0):
+    for arg in args:
+        if isinstance(arg, list):
+           my_list = flat(arg)
+           res += sum(my_list)
+        else:
+            res += arg
+
+    return res
+
+#print(my_sum([[1, 2, [3]], [1], 3]))
+#print(my_sum(1, 2, 3, 4, 5))
+
