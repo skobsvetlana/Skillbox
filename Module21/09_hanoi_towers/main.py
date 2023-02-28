1b7007cdd279cbb91d5d11ec87e60f7bc88039d0
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-28 17:36:46 +0300

    changed Module21/08_hanoi_towers/main.py

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -3,7 +3,6 @@ def move(n, stick2, stick3, stick1=1):
         print(f"Переложить диск {n} со стержня номер {stick1} на стержень номер {stick2}")
         return
 
-
     move(n - 1, stick3, stick2, stick1)
     print(f"Переложить диск {n} со стержня номер {stick1} на стержень номер {stick2}")
     move(n-1, stick2, stick1, stick3)
