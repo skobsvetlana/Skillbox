53af5418d87ac78781ae453532b17261c25b5004
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-02-28 17:27:05 +0300

    changed Module21/08_hanoi_towers/main.py

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def hanoi_tower(n, stick1=1, stick2=3, stick3=2):
+    if n == 1:
+        print(f"Переложить диск {n} со стержня номер {stick1} на стержень номер {stick2}")
+        return
+
+    hanoi_tower(n - 1, stick1, stick3, stick2)
+    print(f"Переложить диск {n} со стержня номер {stick1} на стержень номер {stick2}")
+    hanoi_tower(n-1, stick3, stick2, stick1)
+
+
+n = 5
+hanoi_tower(n)
+
+
