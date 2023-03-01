e52a469691e4f8a580f11b78c7b14c6391adc2f1
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-01 14:42:33 +0300

    changed Module22/01_nums_sum_2

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+file = open('numbers.txt', 'r', encoding='utf-8')
+res = 0
+
+for line in file:
+    res += int(line.strip())
+
+file.close()
+
+file = open('answer.txt', 'w')
+file.write(str(res))
+file.close()
