21a76b15ab7e73c2d4aa3bbda0138bd5f2180ebf
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-04 16:22:17 +0300

    changed Module23/03_lucky_number/main.py

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+import random
+
+def add_to_file(number):
+    with open('result.txt', 'a') as file:
+        file.write(str(number)+ '\n')
+
+
+numbes_sum = 0
+errors = ['NameError', 'TypeError', 'ValueError', 'IndexError']
+
+
+while numbes_sum < 777:
+    number = int(input('Введите число: '))
+    rand_num = random.randint(1, 13)
+    if rand_num == 13:
+        print('Вас постигла неудача!')
+        raise Exception(random.choice(errors))
+    else:
+        add_to_file(number)
+
+    numbes_sum += number
+
+print('Вы успешно выполнили условие для выхода из порочного цикла!')
+
+
+
+
+
+
+
+
