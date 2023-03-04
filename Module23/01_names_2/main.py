0b989fa42ba452f5636642ae21e81a8788f84088
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-04 17:34:37 +0300

    changed Module23/04_registration/main.py

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -2,16 +2,20 @@ import os
 
 file_name = 'people.txt'
 path = os.path.abspath(os.path.join(file_name))
+count = 0
 
 with open(path, 'r', encoding='utf-8') as file:
-    try:
         names = file.read().strip().split()
-        res = len(''.join(names))
         for i, line in enumerate(names):
-            if len(line) < 3:
-                raise ValueError
-    except ValueError:
-        print('Ошибка: менее трёх символов в строке {}.'.format(i + 1))
-    finally:
-        print('Общее количество символов: {}.'.format(res))
+            lenth = len(line)
 
+            try:
+                if lenth < 3:
+                    count += lenth
+                    raise ValueError
+                else:
+                    count += lenth
+            except ValueError:
+                print('Ошибка: менее трёх символов в строке {}.'.format(i + 1))
+
+print('Общее количество символов: {}.'.format(count))
--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+import os
+
+def add_to_file(file, data):
+    with open(file, 'a', encoding='utf-8') as file:
+        file.write(' '.join(data))
+        file.write('\n')
+
+
+file_name = 'registrations.txt'
+path = os.path.abspath(os.path.join(file_name))
+
+with open(path, 'r', encoding='utf-8') as file:
+    for line in file.read().split('\n'):
+        try:
+            data = line.split()
+
+            if not data[0].isalpha():
+                add_to_file('registrations_bad.log', data)
+                raise NameError
+            elif '@' not in data[1] or '.' not in data[1]:
+                add_to_file('registrations_bad.log', data)
+                raise SyntaxError
+            elif int(data[2]) < 10 or int(data[2]) > 100:
+                add_to_file('registrations_bad.log', data)
+                raise ValueError
+            else:
+                add_to_file('registrations_good.log', data)
+        except IndexError:
+            print('НЕ присутствуют все три поля: IndexError')
+        except NameError:
+            print('Поле имени содержит НЕ только буквы: NameError')
+        except SyntaxError:
+            print('Поле «Имейл» НЕ содержит @ и .(точку): SyntaxError')
+        except ValueError:
+            print('Поле «Возраст» НЕ является числом от 10 до 99: ValueError')
