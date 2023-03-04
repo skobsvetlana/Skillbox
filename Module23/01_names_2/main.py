3df4fafa501cd4907d990ae763793fa0a6caa89c
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-04 15:47:27 +0300

    changed Module23/01_names_2/main.py

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+
+file_name = 'people.txt'
+path = os.path.abspath(os.path.join(file_name))
+
+with open(path, 'r', encoding='utf-8') as file:
+    try:
+        names = file.read().strip().split()
+        res = len(''.join(names))
+        for i, line in enumerate(names):
+            if len(line) < 3:
+                raise ValueError
+    except ValueError:
+        print('Ошибка: менее трёх символов в строке {}.'.format(i + 1))
+    finally:
+        print('Общее количество символов: {}.'.format(res))
+
