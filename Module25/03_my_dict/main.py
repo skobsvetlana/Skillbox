8d5ea758d15b31a64127f86a1fec4d215849a6c3
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-13 18:19:54 +0300

    changed Module25/03_my_dict/main.py

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key, default=None):
+        return super().get(key, 0)
+
+
+
+my_dict = MyDict()
+
+my_dict[1] = 'A'
+my_dict[2] = 'B'
+my_dict[3] = 'C'
+my_dict[4] = 'D'
+my_dict[5] = 'E'
+
+print(my_dict.get(1))
+print(my_dict.get(7))
