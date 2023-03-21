aa886d59ec9faf1f2e1857790c258213aeff9327
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-21 17:19:11 +0300

    changed Module26/03_str_count/main.py

--- Module26/04_linked_list/main.py
+++ Module26/04_linked_list/main.py
@@ -1,1 +1,72 @@
-# TODO здесь писать код
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def __str__(self):
+        string = "["
+        node = self.head
+        while node.next:
+            string += str(node.value) + ' '
+            node = node.next
+        string += str(node.value) + ']'
+
+        return string
+
+
+
+    def find_tail(self, head: Node) -> Node:
+        tail = head
+        if tail.next:
+            tail = self.find_tail(head=tail.next)
+
+        return tail
+
+
+    def append(self, node):
+        if not isinstance(node, Node):
+            node = Node(node)
+
+        if self.head:
+            self.find_tail(head=self.head).next = node
+        else:
+            self.head = node
+
+
+    def get(self, ind):
+        curr = self.head
+        for _ in range(ind):
+            curr = curr.next
+
+        return curr
+
+
+    def remove(self, value):
+        curr = self.head
+        for _ in range(value - 1):
+            curr = curr.next
+
+        curr.next = curr.next.next
+
+
+
+my_linked_list = LinkedList()
+my_linked_list.append(10)
+my_linked_list.append(20)
+my_linked_list.append(30)
+
+print('Текущий список:', my_linked_list)
+print('Получение третьего элемента:', my_linked_list.get(2))
+print('Удаление второго элемента.')
+my_linked_list.remove(1)
+print('Новый список:', my_linked_list)
