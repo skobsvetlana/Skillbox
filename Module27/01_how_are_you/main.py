31a8793a96b21d8eabdf6542f12bdd82958bfc62
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-23 21:58:15 +0300

    changed Module27/01_how_are_you/main.py

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+
+def how_are_you(func: Callable, *args, **kwargs) -> Callable:
+    """Декоратор, спрашивающий 'Как дела?' """
+
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> Any:
+        ans = input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        result = func(*args, **kwargs)
+
+        return result
+
+    return wrapped_func
+
+
+@how_are_you
+def test() -> None:
+    '''Какая-то функция'''
+
+    print('<Тут что-то происходит...>')
+
+
+test()
