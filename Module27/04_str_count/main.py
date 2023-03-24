06f22e8733193ca836bce3d5758b889f99c4c3fa
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-24 13:16:37 +0300

    changed Module27/04_str_count/main.py

--- Module27/04_str_count/main.py
+++ Module27/04_str_count/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+class Decorator:
+    def __init__(self, func):
+        self._func = func
+        self.count = 0
+
+
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        result = self._func(*args, **kwargs)
+        print('Функция {func_name} вызывалась {count} раз(а)\n'.format(func_name=self._func.__name__,
+                                                                  count=self.count))
+
+        return result
+
+
+@Decorator
+def test() -> None:
+    '''Какая-то функция'''
+
+    print('<Тут что-то происходит...>')
+
+
+
+test()
+test()
+test()
