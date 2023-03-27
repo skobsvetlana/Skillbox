897b0dc347b46fc2bdb7c50e16d5b034a6e6fc39
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-27 20:50:25 +0300

    changed Module28/01_work_with_file_2/main.py

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+import os
+
+class File:
+    """Модернизированная версия контекст-менеджера File — теперь при попытке
+    открыть несуществующий файл менеджер автоматически создаёт и открывает этот
+    файл в режиме записи.
+    """
+
+    def __init__(self, filename: str, mode: str) -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+
+    def __enter__(self):
+        if not os.path.exists(self.filename):
+            self.file = open(self.filename, 'w')
+            self.file.close()
+
+        self.file = open(self.filename, self.mode)
+
+        return self.file
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+
+with File('example.txt', 'r') as file:
+    file.read()
+
+with File('example.txt', 'w') as file:
+    file.write('Hello')
+
+
+
+
+
