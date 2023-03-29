0e923cd6f5a3820d7b8b17769e110e1c3b9ebfeb
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-03-29 21:13:22 +0300

    changed Module28/03_Date_class/main.py

--- Module28/03_date/Date_class.py
+++ Module28/03_date/Date_class.py
@@ -2,16 +2,24 @@ class Date:
     '''Класс для проверки числа даты на корректность;
     конвертации строки даты в объект класса Date, состоящий из
     соответствующих числовых значений дня, месяца и года.'''
+
+    def __init__(self, day: int = 0, month: int = 0, year: int = 0) -> None:
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self):
+        return 'День: {d}    Месяц: {m}    Год: {y}'.format(d=self.day,
+                                                            m=self.month,
+                                                            y=self.year)
+
     @classmethod
-    def from_string(cls, date: str) -> object:
+    def from_string(cls, date: str) -> 'Date':
         try:
-            day, month, year = tuple(date.split('-'))
-            return 'День: {d}    Месяц: {m}    Год: {y}'.format(d=day,
-                                                                m=month,
-                                                                y=year)
+            day, month, year = map(int, tuple(date.split('-')))
+            return Date(day, month, year)
         except:
             print('Неверный формат даты')
-
 
     @classmethod
     def is_date_valid(cls, date: str) -> bool:
@@ -19,12 +27,12 @@ class Date:
         if len(date) != 3:
             return False
 
-        day, month, year = date
+        day, month, year = map(int, date)
 
-        if int(day) < 1 or int(day) > 31:
+        if 1 < day <= 31:
             return False
 
-        if int(month) < 1 or int(month) > 12:
+        if 1 < month <= 12:
             return False
 
         return True
