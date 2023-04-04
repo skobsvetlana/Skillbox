93e40f4fcdd6a16fb24aeb723f4517f0471c8b50
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-04-04 21:41:53 +0300

    changed Module30/01_zip_again/main.py

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+from typing import List
+from functools import reduce
+
+if __name__ == '__main__':
+    floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+    names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+    numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
+
+    new_floats: List[float] = list(map(lambda x: round(x ** 3, 3), floats))
+    new_names: List[str] = list(filter(lambda x: len(x) >= 5, names))
+    new_number: int = reduce(lambda x, y: x * y, numbers)
+
+    print(new_floats)
+    print(new_names)
+    print(new_number)
+
