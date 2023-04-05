bf05b5f48541acdb244ebc638443ef0b7f45a2a8
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-04-05 14:57:41 +0300

    changed Module31/02_registrations_marks/main.py

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+from typing import List
+
+text = 'А578ВЕ777 ОР233787 К901МН666 К901МZ666 СТ46599 СНИ2929П777 666АМР666'
+
+auto_pattern: str = r'\b[АВЕКМНОРСТУХ]{1}\d{3}[АВЕКМНОРСТУХ]{2}\d{3}\b'
+taxi_pattern: str = r'\b[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}\b'
+
+taxi: List[str] = re.findall(pattern=taxi_pattern, string=text)
+private_auto: List[str] = re.findall(pattern=auto_pattern, string=text)
+
+print('Список номеров частных автомобилей: {}'.format(private_auto))
+print('Список номеров такси: {}'.format(taxi))
