542f4d1f441f37ebbfd17295981e84e1f7dbc7ca
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-04-05 14:43:23 +0300

    changed Module31/01_lorem_ipsum/main.py

--- Module31/01_lorem_ipsum/main.py
+++ Module31/01_lorem_ipsum/main.py
@@ -1,7 +1,13 @@
-text = """ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+import re
+from typing import List
+
+text: str  = """ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
 nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
 Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate 
 """
 
-# TODO здесь писать код
+pattern: str = r'\b\w{4}\b'
+res: List[str] = re.findall(pattern=pattern, string=text)
+
+print(res)
