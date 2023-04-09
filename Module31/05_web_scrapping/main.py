b094c5d9a8d12af27b204306af7641f409fb1e41
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-04-09 16:50:40 +0300

    changed Module31/05_web_scrapping/main.py

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -4,6 +4,7 @@ import requests
 path = 'http://www.columbia.edu/~fdc/sample.html'
 response = requests.get(path)
 text = response.text
+
 pattern = r'<h3.+>(.+)</h3>'
 res = re.findall(pattern=pattern, string=text)
 
