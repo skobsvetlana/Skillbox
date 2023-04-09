48713118256273df64f5e5cef201b7dd8ce562a2
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-04-09 16:05:17 +0300

    changed Module31/04_phone_numbers/main.py

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+
+
+phone_nums = ['9999999999', '999999-999', '99999x9999', '899999']
+pattern = r'[8,9]\d{8}'
+
+for i in range(len(phone_nums)):
+    if re.match(pattern=pattern, string=phone_nums[i]):
+        print(f'{i + 1}-ый номер: всё в порядке')
+    else:
+        print(f'{i + 1}-ый номер: не подходит')
+
