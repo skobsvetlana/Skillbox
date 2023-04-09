a75975bd1434e94c5e93c122d179cae6f11557c2
Svetlana Skobeltcyna <skobsvetlana@gmail.com>
2023-04-09 16:50:05 +0300

    changed Module31/04_phone_numbers/main.py

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import re
+import requests
+
+path = 'http://www.columbia.edu/~fdc/sample.html'
+response = requests.get(path)
+text = response.text
+pattern = r'<h3.+>(.+)</h3>'
+res = re.findall(pattern=pattern, string=text)
+
+print(res)
+
+
